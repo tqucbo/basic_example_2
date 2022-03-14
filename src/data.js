68848f23ba87e8404ec96226d3f1e0ec09b8b835
Trần Quốc Bảo <tqucbo.peter1209@yahoo.com>
2022-03-14 17:40:11 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        id: 1,
+        title: 'What is the difference between Visual Studio Code and Visual Studio IDE?',
+        info: 'Visual Studio Code is a streamlined code editor with support for development operations like debugging, task running, and version control. It aims to provide just the tools a developer needs for a quick code-build-debug cycle and leaves more complex workflows to fuller featured IDEs, such as Visual Studio IDE.',
+    },
+    {
+        id: 2,
+        title: 'Which OSs are supported?',
+        info: 'VS Code runs on macOS, Linux, and Windows. See the Requirements documentation for the supported versions. You can find more platform specific details in the Setup overview.',
+    },
+    {
+        id: 3,
+        title: 'Is VS Code free?',
+        info: 'Yes, VS Code is free for private or commercial use. See the product license for details.',
+    },
+];
+
+export default questions;
